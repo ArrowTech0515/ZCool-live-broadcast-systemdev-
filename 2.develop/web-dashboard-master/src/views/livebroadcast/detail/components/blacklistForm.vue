0da0c80a153e0d1a7e74bf31a1ef1771275194be
ArrowDev0515 <dennis.glushko@gmail.com>
2024-09-02 09:35:12 -0400

    Changed locale to zhCN in datePicker.

--- 2.develop/web-dashboard-master/src/views/livebroadcast/detail/components/blacklistForm.vue
+++ 2.develop/web-dashboard-master/src/views/livebroadcast/detail/components/blacklistForm.vue
@@ -1,57 +1,63 @@
 <template>
-  <a-form :model="formData" style="layout: vertical;">
-    <a-form-item label="禁言用户">
-      <span>{{ formData.username }}</span>
+  <a-config-provider :locale="zhCN">
+    <a-form :model="formData" style="layout: vertical;">
+      <a-form-item label="禁言用户">
+        <span>{{ formData.username }}</span>
+      </a-form-item>
+
+      <a-form-item label="发言内容">
+        <span>{{ formData.content }}</span>
+      </a-form-item>
+
+      <a-form-item label="拉黑类型">
+        <a-checkbox value="account">{{ENUM.block_type[1]}}</a-checkbox>
+        <a-checkbox value="device">{{ENUM.block_type[2]}}</a-checkbox>
+        <a-checkbox value="IP">{{ENUM.block_type[3]}}</a-checkbox>
     </a-form-item>
 
-    <a-form-item label="发言内容">
-      <span>{{ formData.content }}</span>
-    </a-form-item>
+      <a-form-item label="拉黑时效">
+        <a-radio-group v-model:value="formData.muteDuration">
+          <a-radio value="7days">{{ENUM.ageing_type[1]}}</a-radio>
+          <a-radio value="permanent">{{ENUM.ageing_type[2]}}</a-radio>
+          <a-radio value="customize">{{ENUM.ageing_type[3]}}</a-radio>
+        </a-radio-group>
+      </a-form-item>
 
-    <a-form-item label="拉黑类型">
-      <a-checkbox value="account">{{ENUM.block_type[1]}}</a-checkbox>
-      <a-checkbox value="device">{{ENUM.block_type[2]}}</a-checkbox>
-      <a-checkbox value="IP">{{ENUM.block_type[3]}}</a-checkbox>
-   </a-form-item>
+      <a-form-item
+        v-if="formData.muteDuration === 'customize'"
+        label="选择时间"
+        :label-col="{ span: 5 }"
+      >
+        <a-date-picker
+          v-model="formData.end_time"
+          show-time
+          :showTime="{ defaultValue: dayjs('00:00:00', 'HH:mm:ss') }"
+          :disabled-date="disabledDate"
+          format="YYYY-MM-DD HH:mm:ss"
+          value-format="X"
+          placeholder="选择时间"
+        />
+      </a-form-item>
 
-    <a-form-item label="拉黑时效">
-      <a-radio-group v-model:value="formData.muteDuration">
-        <a-radio value="7days">{{ENUM.ageing_type[1]}}</a-radio>
-        <a-radio value="permanent">{{ENUM.ageing_type[2]}}</a-radio>
-        <a-radio value="customize">{{ENUM.ageing_type[3]}}</a-radio>
-      </a-radio-group>
-    </a-form-item>
-
-    <a-form-item
-      v-if="formData.muteDuration === 'customize'"
-      label="选择时间"
-      :label-col="{ span: 5 }"
-    >
-      <a-date-picker
-        v-model="formData.end_time"
-        show-time
-        :showTime="{ defaultValue: dayjs('00:00:00', 'HH:mm:ss') }"
-        :disabled-date="disabledDate"
-        format="YYYY-MM-DD HH:mm:ss"
-        value-format="X"
-        placeholder="选择时间"
-      />
-    </a-form-item>
-
-    <a-form-item label="理由">
-      <a-textarea v-model:value="formData.reason" placeholder="请输入禁言理由" />
-    </a-form-item>
-  </a-form>
+      <a-form-item label="理由">
+        <a-textarea v-model:value="formData.reason" placeholder="请输入禁言理由" />
+      </a-form-item>
+    </a-form>
+  </a-config-provider>
 </template>
 
 <script lang="jsx" setup>
 
 import dayjs from 'dayjs'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'; // Import Chinese locale
 
 const props = defineProps({
   formData: {
     type: Object,
     required: true
   }
-});
+})
+const disabledDate = (current) => {
+  return current && current < dayjs().endOf('day');
+}
 </script>
--- 2.develop/web-dashboard-master/src/views/livebroadcast/detail/components/muteForm.vue
+++ 2.develop/web-dashboard-master/src/views/livebroadcast/detail/components/muteForm.vue
@@ -1,54 +1,60 @@
 <template>
-  <a-form :model="formData" style="layout: vertical;">
-    <a-form-item label="禁言用户">
-      <span>{{ formData.username }}</span>
-    </a-form-item>
+  <a-config-provider :locale="zhCN">
+    <a-form :model="formData" style="layout: vertical;">
+      <a-form-item label="禁言用户">
+        <span>{{ formData.username }}</span>
+      </a-form-item>
 
-    <a-form-item label="发言内容">
-      <span>{{ formData.content }}</span>
-    </a-form-item>
+      <a-form-item label="发言内容">
+        <span>{{ formData.content }}</span>
+      </a-form-item>
 
-    <a-form-item label="禁言时长">
-      <a-radio-group v-model:value="formData.muteDuration">
-        <a-radio value="7days">{{ENUM.ageing_type[1]}}</a-radio>
-        <a-radio value="permanent">{{ENUM.ageing_type[2]}}</a-radio>
-        <a-radio value="customize">{{ENUM.ageing_type[3]}}</a-radio>
-      </a-radio-group>
-      <a-typography-text type="secondary" style="font-size: 10px; color: gray; display: block; margin-top: 8px;">
-        在禁言期间，用户不可发言，其他功能正常使用
-      </a-typography-text>
-    </a-form-item>
+      <a-form-item label="禁言时长">
+        <a-radio-group v-model:value="formData.muteDuration">
+          <a-radio value="7days">{{ENUM.ageing_type[1]}}</a-radio>
+          <a-radio value="permanent">{{ENUM.ageing_type[2]}}</a-radio>
+          <a-radio value="customize">{{ENUM.ageing_type[3]}}</a-radio>
+        </a-radio-group>
+        <a-typography-text type="secondary" style="font-size: 10px; color: gray; display: block; margin-top: 8px;">
+          在禁言期间，用户不可发言，其他功能正常使用
+        </a-typography-text>
+      </a-form-item>
 
-    <a-form-item
-      v-if="formData.muteDuration === 'customize'"
-      label="选择时间"
-      :label-col="{ span: 5 }"
-    >
-      <a-date-picker
-        v-model="formData.end_time"
-        show-time
-        :showTime="{ defaultValue: dayjs('00:00:00', 'HH:mm:ss') }"
-        :disabled-date="disabledDate"
-        format="YYYY-MM-DD HH:mm:ss"
-        value-format="X"
-        placeholder="选择时间"
-      />
-    </a-form-item>
+      <a-form-item
+        v-if="formData.muteDuration === 'customize'"
+        label="选择时间"
+        :label-col="{ span: 5 }"
+      >
+        <a-date-picker
+          v-model="formData.end_time"
+          show-time
+          :showTime="{ defaultValue: dayjs('00:00:00', 'HH:mm:ss') }"
+          :disabled-date="disabledDate"
+          format="YYYY-MM-DD HH:mm:ss"
+          value-format="X"
+          placeholder="选择时间"
+        />
+      </a-form-item>
 
-    <a-form-item label="理由">
-      <a-textarea v-model:value="formData.reason" placeholder="请输入禁言理由" />
-    </a-form-item>
-  </a-form>
+      <a-form-item label="理由">
+        <a-textarea v-model:value="formData.reason" placeholder="请输入禁言理由" />
+      </a-form-item>
+    </a-form>
+  </a-config-provider>
 </template>
 
 <script lang="jsx" setup>
 
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
+import zhCN from 'ant-design-vue/es/locale/zh_CN'; // Import Chinese locale
 
 const props = defineProps({
   formData: {
     type: Object,
     required: true
   }
-});
+})
+const disabledDate = (current) => {
+  return current && current < dayjs().endOf('day')
+}
 </script>
