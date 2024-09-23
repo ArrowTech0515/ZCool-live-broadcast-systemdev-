1bb2041ee5db25f8af5f5dcaf2f2dc2b498504c9
ArrowDev0515 <dennis.glushko@gmail.com>
2024-09-23 02:05:05 -0400

    Added AddorEdit_Parallel page in rebate agent page.

---
+++ 2.develop/web-dashboard-master/src/views/agent/rebate/AddorEdit_Parallel.vue
@@ -0,0 +1,143 @@
+<template>
+  <a-card class="container-card">
+    <a-row>
+      <a-col class="column-center">
+        <!-- Display Mode -->
+        <div class="title">{{ modeDisplay }}</div>
+
+        <a-form-item label="名称" :label-col="{ span: 4 }">
+          <a-input v-model="name" placeholder="请输入名称" style="width: 80%;" />
+        </a-form-item>
+
+        <div class="description-text">
+          <span>平行代理下所有代理的彩票赔率都一样，根据下级的打码进行分成，最多影响5层，10分钟派发一次.</span>
+        </div>
+
+        <!-- Relation Level -->
+        <a-form-item label="关系层数" :label-col="{ span: 4 }" :wrapper-col="{ span: 3 }">
+          <a-select v-model:value="selectedRelationLevel" value="5">
+            <a-select-option v-for="value in relationOptions" :key="value" :value="value">
+              {{ value }}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+
+        <!-- Updated Table: Rebate Plan Settings -->
+        <div class="section">
+          <a-table
+            :columns="rebateColumns"
+            :dataSource="rebateData"
+            rowKey="id"
+            :pagination="false"
+            bordered
+          />
+        </div>
+
+        <!-- Save Buttons -->
+        <div style="text-align: center; margin-top: 20px;">
+          <a-button type="default" style="margin-right: 16px;" @click="emitBack">取消</a-button>
+          <a-button type="primary" @click="onConfirm">确定</a-button>
+        </div>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
+
+<script lang="jsx" setup>
+import { reactive, ref, computed, defineEmits, defineProps } from 'vue';
+
+// Define the emit function
+const emit = defineEmits(['emit_back']);
+const props = defineProps({
+  mode: {
+    type: Number,
+    default: 2,
+  },
+  item: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+// Computed property to reactively display the mode value from ENUM
+const modeDisplay = computed(() => ENUM.agent_rebate_mode[props.mode]);
+
+const name = ref('');
+
+// Rebate Level Data aligned with the image structure
+const rebateData = reactive([
+  { id: 1, plan: 'A', lottery: '', sports: '', electronic: '', chess: '', fishing: '', vision: '', save: '' },
+  { id: 2, plan: 'B', lottery: '', sports: '', electronic: '', chess: '', fishing: '', vision: '', save: '' },
+  { id: 3, plan: 'C', lottery: '', sports: '', electronic: '', chess: '', fishing: '', vision: '', save: '' },
+  { id: 4, plan: 'D', lottery: '', sports: '', electronic: '', chess: '', fishing: '', vision: '', save: '' },
+  { id: 5, plan: 'E', lottery: '', sports: '', electronic: '', chess: '', fishing: '', vision: '', save: '' },
+]);
+
+const rebateColumns = [
+  { title: '返点方案', dataIndex: 'plan', key: 'plan' },
+  { title: '彩票', dataIndex: 'lottery', key: 'lottery', editable: true },
+  { title: '体育', dataIndex: 'sports', key: 'sports', editable: true },
+  { title: '电子', dataIndex: 'electronic', key: 'electronic', editable: true },
+  { title: '棋牌', dataIndex: 'chess', key: 'chess', editable: true },
+  { title: '捕鱼', dataIndex: 'fishing', key: 'fishing', editable: true },
+  { title: '视讯', dataIndex: 'vision', key: 'vision', editable: true },
+  { title: '存款', dataIndex: 'save', key: 'save', editable: true },
+];
+
+// Select options for '关系层数' from 1 to 10
+const relationOptions = Array.from({ length: 5 }, (_, index) => index + 1);
+const selectedRelationLevel = ref(5); // Default selected value
+
+// Emit the 'emit_back' event on confirmation
+const onConfirm = () => {
+  emit('emit_back');
+};
+
+// Emit function for the cancel button
+const emitBack = () => {
+  emit('emit_back');
+};
+</script>
+
+<style scoped>
+.container-card {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.column-center {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+.title {
+  margin-bottom: 20px;
+  font-size: large;
+  font-weight: bold;
+  text-align: center;
+}
+
+.section {
+  margin-bottom: 24px;
+}
+
+.description-text {
+  color: grey;
+  margin-bottom: 16px;
+}
+
+.button-group {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.ant-form-item-label > label {
+  font-weight: bold !important;
+}
+</style>
+
--- 2.develop/web-dashboard-master/src/views/agent/rebate/index.vue
+++ 2.develop/web-dashboard-master/src/views/agent/rebate/index.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade-slide" mode="out-in">
-    <div v-if="!showAddorEditRebatePage">
+    <div v-if="!showAddorEdit_OddsPage && !showAddorEdit_ParallelPage">
       <div class="page_container">
         <FormSearch
           ref="formSearchRef"
@@ -15,9 +15,17 @@
         />
       </div>
     </div>
-    <div v-else-if="showAddorEditRebatePage">
-      <AddorEditRebate
+    <div v-else-if="showAddorEdit_OddsPage">
+      <AddorEdit_Odds
         @emit_back="onBackToMainPage2"
+        :mode="agent_mode"
+        :item="agent_item"
+        @confirm="handleConfirm"
+        @reject="handleReject" />
+    </div>
+    <div v-else-if="showAddorEdit_ParallelPage">
+      <AddorEdit_Parallel
+        @emit_back="onBackToMainPage3"
         :mode="agent_mode"
         :item="agent_item"
         @confirm="handleConfirm"
@@ -29,20 +37,26 @@
 <script setup lang="jsx">
 import CustomTable from './components/CustomTable.vue'
 import FormSearch from './components/FormSearch.vue'
-import AddorEditRebate from './AddorEditRebate.vue'
+import AddorEdit_Odds from './AddorEdit_Odds.vue'
+import AddorEdit_Parallel from './AddorEdit_Parallel.vue'
 
 const customTableRef = ref(null)
 const formSearchRef = ref(null)
 const searchParams = ref({})
 
-const showAddorEditRebatePage = ref(false) // New state to manage which view to show
+const showAddorEdit_OddsPage = ref(false) // New state to manage which view to show
+const showAddorEdit_ParallelPage = ref(false) // New state to manage which view to show
+
 const agent_mode = ref(2) // New state to manage which view to show
 const agent_item = ref({}) // New state to manage which view to show
 
 const onBackToMainPage2 = () => {
-  showAddorEditRebatePage.value = false // Switch back to the main table view
+  showAddorEdit_OddsPage.value = false // Switch back to the main table view
 }
 
+const onBackToMainPage3 = () => {
+  showAddorEdit_ParallelPage.value = false // Switch back to the main table view
+}
 const onAddRebate = (mode = 2, item = {}) => {
   agent_mode.value = mode  // Use .value to update ref value
   agent_item.value = item  // Use .value to update ref value
@@ -50,7 +64,19 @@ const onAddRebate = (mode = 2, item = {}) => {
   console.log("onAddRebate : " + agent_mode.value)
   console.log("onAddRebate2 : " + agent_item.value)
   
-  showAddorEditRebatePage.value = true // Switch to the add strategy view
+  switch(agent_mode.value) {
+    case 2:
+      showAddorEdit_OddsPage.value = true // Switch to the add strategy view
+      break
+    case 3:
+      showAddorEdit_ParallelPage.value = true // Switch to the add strategy view
+      break
+    case 4:
+      showAddorEdit_ParallelPage.value = true // Switch to the add strategy view
+      break
+    default:
+      break
+  }
 }
 
 </script>
