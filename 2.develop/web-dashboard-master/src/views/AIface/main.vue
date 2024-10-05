d9d1fbfc912a8dd251d531f17d204d76891d52e1
ArrowDev0515 <dennis.glushko@gmail.com>
2024-10-05 11:24:17 -0400

    Enabled water AI face page.

--- 2.develop/web-dashboard-master/src/views/AIface/main.vue
+++ 2.develop/web-dashboard-master/src/views/AIface/main.vue
@@ -2,12 +2,12 @@
     <a-tabs default-active-key="1">
         
       <a-tab-pane key="1" tab="人脸配置">
-          <configPage />
+        <configPage />
       </a-tab-pane>
         
-        <!-- <a-tab-pane key="2" tab="人脸流水">
-            <waterPage />
-        </a-tab-pane> -->
+      <a-tab-pane key="2" tab="人脸流水">
+        <waterPage />
+      </a-tab-pane>
 
     </a-tabs>
   </template>
